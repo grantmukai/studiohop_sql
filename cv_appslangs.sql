5cadec65588fe97a78f8ea088966b6ab8ff056e7
El Capitan Macbook Air <grantmukai@icloud.com>
2016-02-22 18:58:56 -0500

    sauvegarde

--- cv_appslangs.sql
+++ cv_appslangs.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: cv
 -- ------------------------------------------------------
--- Server version	5.6.28
+-- Server version	5.6.29
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -50,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-02-20 13:06:33
+-- Dump completed on 2016-02-22 18:50:03
